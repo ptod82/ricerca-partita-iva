f8907cd4091f0d9d66ce0dde68cb623eb1bebfad
paolo todini <paolotodini@paolos-MacBook-Pro.local>
2018-01-03 16:58:51 +0100

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Ricerca Partita Iva</h1>
+                <div></div>
+            </div>
+        )
+    }
+}
+
+export default App;
